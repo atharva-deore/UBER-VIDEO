15e27f3e67a5f662a9289533a60cf3d16600fb1e
Atharva Deore <atharvadeore0032@gmail.com>
2025-01-04 23:03:10 +0530

    user authentication done

--- Backend/app.js
+++ Backend/app.js
@@ -2,13 +2,23 @@ const dotenv =require('dotenv');
 dotenv.config();
 const express =require('express');
 const app =express();
+const cookieParser=require('cookie-parser');
+const userRoutes=require('./routes/user.routes');
+
+const connectToDb=require('./db/db');
+connectToDb();
 
 const cors=require('cors');
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
 
 app.get('/',(req,res)=>{
     res.send('Hello World');
 });
+
+app.use('/user',userRoutes);
 
 module.exports=app;
